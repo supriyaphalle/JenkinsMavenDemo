10d133b9d8e344d1ccabcf46dd413bccfbbd5441
supriyaphalle <supriyaphalle@gmail.com>
2020-07-07 17:53:32 +0530

    ADDED: readme file

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-    @GetMapping("/books")
+    @GetMapping("/hello")
     public String getAllData() {
             return "Hello World";
     }
